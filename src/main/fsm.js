d54bccb13ac27c87dcc3ff6946c442378777f587
Nico Neubig <nico@neubig.name>
2021-04-25 23:23:45 +0200

    Project Feature

--- src/main/fsm.js
+++ src/main/fsm.js
@@ -1,15 +1,15 @@
-var greekLetterNames = [ 'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega' ];
+var greekLetterNames = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega'];
 
 function convertLatexShortcuts(text) {
 	// html greek characters
-	for(var i = 0; i < greekLetterNames.length; i++) {
+	for (var i = 0; i < greekLetterNames.length; i++) {
 		var name = greekLetterNames[i];
 		text = text.replace(new RegExp('\\\\' + name, 'g'), String.fromCharCode(913 + i + (i > 16)));
 		text = text.replace(new RegExp('\\\\' + name.toLowerCase(), 'g'), String.fromCharCode(945 + i + (i > 16)));
 	}
 
 	// subscripts
-	for(var i = 0; i < 10; i++) {
+	for (var i = 0; i < 10; i++) {
 		text = text.replace(new RegExp('_' + i, 'g'), String.fromCharCode(8320 + i));
 	}
 
@@ -19,9 +19,9 @@ function convertLatexShortcuts(text) {
 function textToXML(text) {
 	text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 	var result = '';
-	for(var i = 0; i < text.length; i++) {
+	for (var i = 0; i < text.length; i++) {
 		var c = text.charCodeAt(i);
-		if(c >= 0x20 && c <= 0x7E) {
+		if (c >= 0x20 && c <= 0x7E) {
 			result += text[i];
 		} else {
 			result += '&#' + c + ';';
@@ -53,7 +53,7 @@ function drawText(c, originalText, x, y, angleOrNull, isSelected) {
 	x -= width / 2;
 
 	// position the text intelligently if given an angle
-	if(angleOrNull != null) {
+	if (angleOrNull != null) {
 		var cos = Math.cos(angleOrNull);
 		var sin = Math.sin(angleOrNull);
 		var cornerPointX = (width / 2 + 5) * (cos > 0 ? 1 : -1);
@@ -64,13 +64,13 @@ function drawText(c, originalText, x, y, angleOrNull, isSelected) {
 	}
 
 	// draw text and caret (round the coordinates so the caret falls on a pixel)
-	if('advancedFillText' in c) {
+	if ('advancedFillText' in c) {
 		c.advancedFillText(text, originalText, x + width / 2, y, angleOrNull);
 	} else {
 		x = Math.round(x);
 		y = Math.round(y);
 		c.fillText(text, x, y + 6);
-		if(isSelected && caretVisible && canvasHasFocus() && document.hasFocus()) {
+		if (isSelected && caretVisible && canvasHasFocus() && document.hasFocus()) {
 			x += width;
 			c.beginPath();
 			c.moveTo(x, y - 10);
@@ -107,17 +107,17 @@ function drawUsing(c) {
 	c.save();
 	c.translate(0.5, 0.5);
 
-	for(var i = 0; i < nodes.length; i++) {
+	for (var i = 0; i < nodes.length; i++) {
 		c.lineWidth = 1;
 		c.fillStyle = c.strokeStyle = (nodes[i] == selectedObject) ? 'blue' : 'black';
 		nodes[i].draw(c);
 	}
-	for(var i = 0; i < links.length; i++) {
+	for (var i = 0; i < links.length; i++) {
 		c.lineWidth = 1;
 		c.fillStyle = c.strokeStyle = (links[i] == selectedObject) ? 'blue' : 'black';
 		links[i].draw(c);
 	}
-	if(currentLink != null) {
+	if (currentLink != null) {
 		c.lineWidth = 1;
 		c.fillStyle = c.strokeStyle = 'black';
 		currentLink.draw(c);
@@ -132,13 +132,13 @@ function draw() {
 }
 
 function selectObject(x, y) {
-	for(var i = 0; i < nodes.length; i++) {
-		if(nodes[i].containsPoint(x, y)) {
+	for (var i = 0; i < nodes.length; i++) {
+		if (nodes[i].containsPoint(x, y)) {
 			return nodes[i];
 		}
 	}
-	for(var i = 0; i < links.length; i++) {
-		if(links[i].containsPoint(x, y)) {
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].containsPoint(x, y)) {
 			return links[i];
 		}
 	}
@@ -146,48 +146,48 @@ function selectObject(x, y) {
 }
 
 function snapNode(node) {
-	for(var i = 0; i < nodes.length; i++) {
-		if(nodes[i] == node) continue;
+	for (var i = 0; i < nodes.length; i++) {
+		if (nodes[i] == node) continue;
 
-		if(Math.abs(node.x - nodes[i].x) < snapToPadding) {
+		if (Math.abs(node.x - nodes[i].x) < snapToPadding) {
 			node.x = nodes[i].x;
 		}
 
-		if(Math.abs(node.y - nodes[i].y) < snapToPadding) {
+		if (Math.abs(node.y - nodes[i].y) < snapToPadding) {
 			node.y = nodes[i].y;
 		}
 	}
 }
 
-window.onload = function() {
+window.onload = function () {
 	canvas = document.getElementById('canvas');
 	restoreBackup();
 	draw();
 
-	canvas.onmousedown = function(e) {
+	canvas.onmousedown = function (e) {
 		var mouse = crossBrowserRelativeMousePos(e);
 		selectedObject = selectObject(mouse.x, mouse.y);
 		movingObject = false;
 		originalClick = mouse;
 
-		if(selectedObject != null) {
-			if(shift && selectedObject instanceof Node) {
+		if (selectedObject != null) {
+			if (shift && selectedObject instanceof Node) {
 				currentLink = new SelfLink(selectedObject, mouse);
 			} else {
 				movingObject = true;
 				deltaMouseX = deltaMouseY = 0;
-				if(selectedObject.setMouseStart) {
+				if (selectedObject.setMouseStart) {
 					selectedObject.setMouseStart(mouse.x, mouse.y);
 				}
 			}
 			resetCaret();
-		} else if(shift) {
+		} else if (shift) {
 			currentLink = new TemporaryLink(mouse, mouse);
 		}
 
 		draw();
 
-		if(canvasHasFocus()) {
+		if (canvasHasFocus()) {
 			// disable drag-and-drop only if the canvas is already focused
 			return false;
 		} else {
@@ -197,40 +197,40 @@ window.onload = function() {
 		}
 	};
 
-	canvas.ondblclick = function(e) {
+	canvas.ondblclick = function (e) {
 		var mouse = crossBrowserRelativeMousePos(e);
 		selectedObject = selectObject(mouse.x, mouse.y);
 
-		if(selectedObject == null) {
+		if (selectedObject == null) {
 			selectedObject = new Node(mouse.x, mouse.y);
 			nodes.push(selectedObject);
 			resetCaret();
 			draw();
-		} else if(selectedObject instanceof Node) {
+		} else if (selectedObject instanceof Node) {
 			selectedObject.isAcceptState = !selectedObject.isAcceptState;
 			draw();
 		}
 	};
 
-	canvas.onmousemove = function(e) {
+	canvas.onmousemove = function (e) {
 		var mouse = crossBrowserRelativeMousePos(e);
 
-		if(currentLink != null) {
+		if (currentLink != null) {
 			var targetNode = selectObject(mouse.x, mouse.y);
-			if(!(targetNode instanceof Node)) {
+			if (!(targetNode instanceof Node)) {
 				targetNode = null;
 			}
 
-			if(selectedObject == null) {
-				if(targetNode != null) {
+			if (selectedObject == null) {
+				if (targetNode != null) {
 					currentLink = new StartLink(targetNode, originalClick);
 				} else {
 					currentLink = new TemporaryLink(originalClick, mouse);
 				}
 			} else {
-				if(targetNode == selectedObject) {
+				if (targetNode == selectedObject) {
 					currentLink = new SelfLink(selectedObject, mouse);
-				} else if(targetNode != null) {
+				} else if (targetNode != null) {
 					currentLink = new Link(selectedObject, targetNode);
 				} else {
 					currentLink = new TemporaryLink(selectedObject.closestPointOnCircle(mouse.x, mouse.y), mouse);
@@ -239,20 +239,20 @@ window.onload = function() {
 			draw();
 		}
 
-		if(movingObject) {
+		if (movingObject) {
 			selectedObject.setAnchorPoint(mouse.x, mouse.y);
-			if(selectedObject instanceof Node) {
+			if (selectedObject instanceof Node) {
 				snapNode(selectedObject);
 			}
 			draw();
 		}
 	};
 
-	canvas.onmouseup = function(e) {
+	canvas.onmouseup = function (e) {
 		movingObject = false;
 
-		if(currentLink != null) {
-			if(!(currentLink instanceof TemporaryLink)) {
+		if (currentLink != null) {
+			if (!(currentLink instanceof TemporaryLink)) {
 				selectedObject = currentLink;
 				links.push(currentLink);
 				resetCaret();
@@ -265,16 +265,16 @@ window.onload = function() {
 
 var shift = false;
 
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
 	var key = crossBrowserKey(e);
 
-	if(key == 16) {
+	if (key == 16) {
 		shift = true;
-	} else if(!canvasHasFocus()) {
+	} else if (!canvasHasFocus()) {
 		// don't read keystrokes when other things have focus
 		return true;
-	} else if(key == 8) { // backspace key
-		if(selectedObject != null && 'text' in selectedObject) {
+	} else if (key == 8) { // backspace key
+		if (selectedObject != null && 'text' in selectedObject) {
 			selectedObject.text = selectedObject.text.substr(0, selectedObject.text.length - 1);
 			resetCaret();
 			draw();
@@ -282,15 +282,15 @@ document.onkeydown = function(e) {
 
 		// backspace is a shortcut for the back button, but do NOT want to change pages
 		return false;
-	} else if(key == 46) { // delete key
-		if(selectedObject != null) {
-			for(var i = 0; i < nodes.length; i++) {
-				if(nodes[i] == selectedObject) {
+	} else if (key == 46) { // delete key
+		if (selectedObject != null) {
+			for (var i = 0; i < nodes.length; i++) {
+				if (nodes[i] == selectedObject) {
 					nodes.splice(i--, 1);
 				}
 			}
-			for(var i = 0; i < links.length; i++) {
-				if(links[i] == selectedObject || links[i].node == selectedObject || links[i].nodeA == selectedObject || links[i].nodeB == selectedObject) {
+			for (var i = 0; i < links.length; i++) {
+				if (links[i] == selectedObject || links[i].node == selectedObject || links[i].nodeA == selectedObject || links[i].nodeB == selectedObject) {
 					links.splice(i--, 1);
 				}
 			}
@@ -300,28 +300,28 @@ document.onkeydown = function(e) {
 	}
 };
 
-document.onkeyup = function(e) {
+document.onkeyup = function (e) {
 	var key = crossBrowserKey(e);
 
-	if(key == 16) {
+	if (key == 16) {
 		shift = false;
 	}
 };
 
-document.onkeypress = function(e) {
+document.onkeypress = function (e) {
 	// don't read keystrokes when other things have focus
 	var key = crossBrowserKey(e);
-	if(!canvasHasFocus()) {
+	if (!canvasHasFocus()) {
 		// don't read keystrokes when other things have focus
 		return true;
-	} else if(key >= 0x20 && key <= 0x7E && !e.metaKey && !e.altKey && !e.ctrlKey && selectedObject != null && 'text' in selectedObject) {
+	} else if (key >= 0x20 && key <= 0x7E && !e.metaKey && !e.altKey && !e.ctrlKey && selectedObject != null && 'text' in selectedObject) {
 		selectedObject.text += String.fromCharCode(key);
 		resetCaret();
 		draw();
 
 		// don't let keys do their actions (like space scrolls down the page)
 		return false;
-	} else if(key == 8) {
+	} else if (key == 8) {
 		// backspace is a shortcut for the back button, but do NOT want to change pages
 		return false;
 	}
@@ -336,7 +336,7 @@ function crossBrowserElementPos(e) {
 	e = e || window.event;
 	var obj = e.target || e.srcElement;
 	var x = 0, y = 0;
-	while(obj.offsetParent) {
+	while (obj.offsetParent) {
 		x += obj.offsetLeft;
 		y += obj.offsetTop;
 		obj = obj.offsetParent;
@@ -396,4 +396,22 @@ function saveAsLaTeX() {
 	selectedObject = oldSelectedObject;
 	var texData = exporter.toLaTeX();
 	output(texData);
+}
+
+const clearCanvas = () => {
+	localStorage['fsm'] = '';
+	nodes = [];
+	links = [];
+	draw();
+
+}
+
+const clearNodes = () => {
+	for (var i = 0; i < nodes.length; i++) {
+		nodes[i].text = '';
+	}
+	for (var i = 0; i < links.length; i++) {
+		links[i].text = '';
+	}
+	draw();
 }
--- src/main/save.js
+++ src/main/save.js
@@ -1,56 +1,135 @@
-function restoreBackup() {
-	if(!localStorage || !JSON) {
+let fsmList = [
+	{ name: 'fsm_0', content: null },
+];
+let currentFsm = 0;
+let restore = false;
+let exportFileName = 'Finite State Machine Project';
+
+window.onLoad = () => {
+	updateFsmListLoad();
+}
+
+const onClickFsmItem = (index) => {
+	currentFsm = index;
+	restoreBackup(fsmList[index].content);
+	updateFsmListLoad();
+	draw();
+}
+
+const renameFsmItem = (index) => {
+	const name = prompt("Enter a name:", fsmList[index].name);
+	if (name) {
+		fsmList[index].name = name;
+		updateFsmListLoad();
+	}
+}
+
+const updateFsmListLoad = () => {
+	const list = document.getElementById('fsmList');
+	while (list.firstChild) {
+		list.removeChild(list.lastChild);
+	}
+	for (let i = 0; i < fsmList.length; i++) {
+		const listItem = document.createElement("div");
+		listItem.appendChild(document.createTextNode(fsmList[i].name));
+		if (i === currentFsm) listItem.className = "selected"
+		listItem.addEventListener('click', () => onClickFsmItem(i));
+		listItem.addEventListener('dblclick', () => renameFsmItem(i));
+		list.appendChild(listItem);
+	}
+}
+
+const addFSM = () => {
+	fsmList.push({ name: `fsm_${fsmList.length}`, content: null });
+	updateFsmListLoad();
+	clearCanvas();
+}
+
+function exportFsmList() {
+	const filename = prompt("Enter an file name:", exportFileName);
+	if (filename == null) return;
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(fsmList)));
+	element.setAttribute('download', filename);
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
+
+function importFsmList() {
+	restore = true;
+	const fileInput = document.getElementById('fileInput');
+	if (fileInput.files.length === 0) {
+		console.log('No file selected.');
+		return;
+	}
+	const reader = new FileReader();
+	reader.onload = function fileReadCompleted() {
+		exportFileName = fileInput.files[0].name;
+		fsmList = JSON.parse(reader.result);
+		currentFsm = 0;
+		restoreBackup(fsmList[currentFsm].content);
+		updateFsmListLoad();
+		draw();
+		restore = false;
+	};
+	reader.readAsText(fileInput.files[0]);
+};
+
+function restoreBackup(fsmBackup = null) {
+	nodes = [];
+	links = [];
+	if (!localStorage && !fsmBackup) {
 		return;
 	}
 
 	try {
-		var backup = JSON.parse(localStorage['fsm']);
+		var backup = fsmBackup || JSON.parse(localStorage['fsm']);
 
-		for(var i = 0; i < backup.nodes.length; i++) {
+		for (var i = 0; i < backup.nodes.length; i++) {
 			var backupNode = backup.nodes[i];
 			var node = new Node(backupNode.x, backupNode.y);
 			node.isAcceptState = backupNode.isAcceptState;
 			node.text = backupNode.text;
 			nodes.push(node);
 		}
-		for(var i = 0; i < backup.links.length; i++) {
+		for (var i = 0; i < backup.links.length; i++) {
 			var backupLink = backup.links[i];
 			var link = null;
-			if(backupLink.type == 'SelfLink') {
+			if (backupLink.type == 'SelfLink') {
 				link = new SelfLink(nodes[backupLink.node]);
 				link.anchorAngle = backupLink.anchorAngle;
 				link.text = backupLink.text;
-			} else if(backupLink.type == 'StartLink') {
+			} else if (backupLink.type == 'StartLink') {
 				link = new StartLink(nodes[backupLink.node]);
 				link.deltaX = backupLink.deltaX;
 				link.deltaY = backupLink.deltaY;
 				link.text = backupLink.text;
-			} else if(backupLink.type == 'Link') {
+			} else if (backupLink.type == 'Link') {
 				link = new Link(nodes[backupLink.nodeA], nodes[backupLink.nodeB]);
 				link.parallelPart = backupLink.parallelPart;
 				link.perpendicularPart = backupLink.perpendicularPart;
 				link.text = backupLink.text;
 				link.lineAngleAdjust = backupLink.lineAngleAdjust;
 			}
-			if(link != null) {
+			if (link != null) {
 				links.push(link);
 			}
 		}
-	} catch(e) {
+	} catch (e) {
 		localStorage['fsm'] = '';
 	}
 }
 
 function saveBackup() {
-	if(!localStorage || !JSON) {
-		return;
-	}
+	if (restore) return;
 
 	var backup = {
 		'nodes': [],
 		'links': [],
 	};
-	for(var i = 0; i < nodes.length; i++) {
+	for (var i = 0; i < nodes.length; i++) {
 		var node = nodes[i];
 		var backupNode = {
 			'x': node.x,
@@ -60,17 +139,17 @@ function saveBackup() {
 		};
 		backup.nodes.push(backupNode);
 	}
-	for(var i = 0; i < links.length; i++) {
+	for (var i = 0; i < links.length; i++) {
 		var link = links[i];
 		var backupLink = null;
-		if(link instanceof SelfLink) {
+		if (link instanceof SelfLink) {
 			backupLink = {
 				'type': 'SelfLink',
 				'node': nodes.indexOf(link.node),
 				'text': link.text,
 				'anchorAngle': link.anchorAngle,
 			};
-		} else if(link instanceof StartLink) {
+		} else if (link instanceof StartLink) {
 			backupLink = {
 				'type': 'StartLink',
 				'node': nodes.indexOf(link.node),
@@ -78,7 +157,7 @@ function saveBackup() {
 				'deltaX': link.deltaX,
 				'deltaY': link.deltaY,
 			};
-		} else if(link instanceof Link) {
+		} else if (link instanceof Link) {
 			backupLink = {
 				'type': 'Link',
 				'nodeA': nodes.indexOf(link.nodeA),
@@ -89,9 +168,15 @@ function saveBackup() {
 				'perpendicularPart': link.perpendicularPart,
 			};
 		}
-		if(backupLink != null) {
+		if (backupLink != null) {
 			backup.links.push(backupLink);
 		}
+	}
+	fsmList[currentFsm].content = backup;
+
+	updateFsmListLoad();
+	if (!localStorage || !JSON) {
+		return;
 	}
 
 	localStorage['fsm'] = JSON.stringify(backup);
